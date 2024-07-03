3443df4666a3d76e780fdc96c3988b252187966e
EWimmer <elisabeth.wimmer@uni-ulm.de>
2024-07-03 05:24:55 +0200

    Feat: Calculates driving performance factors based on choices made in HUD creation

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import random
 import time
 import xml.dom.minidom as minidom
+import calculations
 
 # Basisverzeichnis für CARLA und die Konfigurationsdatei
 carla_base_dir = r"C:\Users\wimme\Downloads\CARLA\WindowsNoEditor"
@@ -79,71 +80,24 @@ def start_simulation():
             start_sumo(selected_sumocfg)
             print("Simulation nicht gestartet, da die Checkbox nicht angekreuzt ist.")
 
+
 def hudSelection():
+    experience_level= 5
+    age = 30
+
     for hud in hud_frames:
         probability = hud['entry'].get()
         brightness_level = hud['brightness_var'].get()
         information_density = hud['density_var'].get()
         information_relevance = hud['relevance_var'].get()
         fov_selection = hud['fov_var'].get()
-        calc_distraction(information_relevance, fov_selection, information_density, brightness_level)
+        distraction_level = calculations.calc_distraction(information_relevance, fov_selection, information_density, brightness_level)
+        fatigueness_level = calculations.calc_fatigueness(information_relevance, fov_selection, information_density)
+        awareness_level = calculations.calc_awareness(fov_selection, information_relevance, information_density, distraction_level, fatigueness_level)
+        reactTime = calculations.calc_ReactTime(distraction_level, fatigueness_level, experience_level, awareness_level, age)
 
-        
         print(f"HUD attributes - Probability: {probability}, Brightness Level: {brightness_level}, Information Density: {information_density}, Information Relevance: {information_relevance}, FoV Selection: {fov_selection}")
-
-
-def calc_distraction(information_relevance, fov_selection, information_density, brightness_level):
-
-    relevance_value = 0
-    fov_value= 0
-    density_value = 0
-    brightness_value = 0
-    
-    if information_relevance == "Unwichtig" :
-        relevance_value = 4
-    elif information_relevance == "Neutral":
-        relevance_value = 2
-    elif information_relevance == "Wichtig":
-        relevance_value = 1
-    else: 
-        relevance_value = 3
-
-    if fov_selection == "Small" :
-        fov_value = 4
-    elif fov_selection == "Medium":
-        fov_value = 1
-    elif fov_selection == "Large":
-        fov_value = 3
-    else: 
-        fov_value = 2    
-
-    if information_density == "Minimum" :
-        density_value = 1
-    elif information_density == "Moderat":
-        density_value = 3
-    elif information_density == "Maximum":
-        density_value = 5
-    else: 
-        density_value = 2
-
-    if brightness_level == "Sehr dunkel" :
-        brightness_value = 5
-    elif brightness_level == "Dunkel":
-        brightness_value = 3
-    elif brightness_level == "Moderat":
-        brightness_value = 2
-    elif brightness_level == "Hell":
-        brightness_value = 4
-    elif brightness_level == "Sehr hell":
-        brightness_value = 5     
-    else: 
-        brightness_value = 2    
-
-    base_distraction = 5
-
-    distraction_level = base_distraction + 0.15 * brightness_value + 0.35 * density_value + 0.35 * relevance_value + 0.15 * fov_value     
-
-    print("The distraction level is: " + str(distraction_level))
+        print("Calculated: distraction: " + str(distraction_level) + " fatigueness: " + str(fatigueness_level) + " awareness: " + str(awareness_level) + " reacTime: " + str(reactTime))
 
 
 def start_sumo(selected_sumocfg):
