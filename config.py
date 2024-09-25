7ff1fba2b9e1d95be2ea462a0ef921cb2e5eddb9
Jan Maresch <jan.maresch@uni-ulm.de>
2024-09-25 22:42:27 +0200

    config

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 carla_base_dir = r"F:\Softwareprojekt\CARLA_0.9.15\WindowsNoEditor"
 #carla_base_dir = r"C:\Users\wimme\Downloads\CARLA\WindowsNoEditor"
-tracipath = r"C:\Users\wimme\Downloads\CARLA\WindowsNoEditor\Co-Simulation\Sumo\examples\Town01.sumocfg"
+tracipath = r"F:\Softwareprojekt\CARLA_0.9.15\WindowsNoEditor\Co-Simulation\Sumo\examples\Town01.sumocfg"
+#tracipath = r"C:\Users\wimme\Downloads\CARLA\WindowsNoEditor\Co-Simulation\Sumo\examples\Town01.sumocfg"
